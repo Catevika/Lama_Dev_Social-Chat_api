a964178c85c7ddb5304a335105ddf5e5cd28ac55
Catevika <dominique.bello@outlook.fr>
2022-11-04 12:38:36 +0100

    Add userRoute

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -58,6 +58,45 @@ router.get('/', async (req, res) => {
 	}
 });
 
+// get all users from the database
+router.get('/all', async (req, res) => {
+	try {
+		const data = await UserModel.find();
+		if (!data) {
+			res.status(404).json('People not found');
+		}
+		res.status(200).json(data);
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+});
+
+// get following as friends
+router.get('/friends/:userId', async (req, res) => {
+	try {
+		const user = await UserModel.findById(req.params.userId);
+		const friends = await Promise.all(
+			user.following.map((followingId) => {
+				return UserModel.findById(followingId);
+			})
+		);
+
+		let friendList = [];
+		friends.map((friend) => {
+			const { _id, username, profilePicture } = friend;
+			friendList.push({ _id, username, profilePicture });
+		});
+
+		if (friendList === []) {
+			res.status(404).json('No friend yet');
+		} else {
+			res.status(200).json(friendList);
+		}
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+});
+
 // Follow a user
 router.put('/:id/follow', async (req, res) => {
 	if (req.body.userId !== req.params.id) {
